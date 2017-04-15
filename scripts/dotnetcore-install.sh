2989845f6f132f08b51539f783dc9ef9db416eb6
antoineawad <tonyawad88@hotmail.com>
2017-04-15 10:53:01 -0400

    updated dotnetcore script

--- scripts/dotnetcore-install.sh
+++ scripts/dotnetcore-install.sh
@@ -1,4 +1,4 @@
-sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ yakkety main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
 sudo apt-get update
 
