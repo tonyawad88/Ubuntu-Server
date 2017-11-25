44f5eabe0fd7b0dfb1d6f9b58b6fe7500c209898
tonyawad <tonyawad88@gmail.com>
2017-11-25 15:07:32 -0500

    webmin enable two factor authentication

---
+++ scripts/webmin-enable-two-factor-auth.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install libauthen-oath-perl
+
+#After this, do the following:
+#1. Open Webmin
+#2. Go to Webmin => Webmin Configuration
+#3. Select Two Factor Authentication
+#4. Enable Google Authenticator
