faa89360e87acefa85a7b6a852f26df13cd5840a
tonyawad88 <tonyawad88@hotmail.com>
2018-10-28 22:20:08 -0400

    modifies github repo to store

--- scripts/github/setup-github-repo.sh
+++ scripts/github/setup-github-repo.sh
@@ -1,4 +1,4 @@
 #The following will help enter the password only once
 git config user.name "tonyawad88"
 git config user.email "tonyawad88@hotmail.com"
-git config credential.helper cache
+git config credential.helper store
